a3a8093e9f96870eec66a041e547d41842a3eb26
Alexander Martinz <amartinz@shiftphones.com>
2024-12-05 11:42:04 +0100

    (SHIFT) otter: add full target
    This allows inheriting from full_otter.mk to deduplicate more
    properties for non-lineage builds.
    
    Change-Id: Ibbf1fd55bbdb47aca637c833cf1a5dc02c82d6d2
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -3,6 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
@@ -83,6 +88,10 @@ PRODUCT_COPY_FILES += \
 
 # Board API level
 BOARD_SHIPPING_API_LEVEL := 30
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
 
 # Boot control
 PRODUCT_PACKAGES += \
