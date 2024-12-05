63c0198893060090b4252fde1f11c839f15b8851
Alexander Martinz <amartinz@shiftphones.com>
2024-12-05 11:42:04 +0100

    (SHIFT) otter: add full target
    This allows inheriting from full_otter.mk to deduplicate more
    properties for non-lineage builds.
    
    Change-Id: Ibbf1fd55bbdb47aca637c833cf1a5dc02c82d6d2
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- lineage_otter.mk
+++ lineage_otter.mk
@@ -3,29 +3,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-TARGET_SUPPORTS_OMX_SERVICE := false
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from otter device
-$(call inherit-product, device/shift/otter/device.mk)
+# Inherit from full device.
+$(call inherit-product, device/shift/otter/full_otter.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
-
-# Device identifier. This must come after all inclusions.
+# Override product name for Lineage.
 PRODUCT_NAME := lineage_otter
-PRODUCT_DEVICE := otter
-PRODUCT_BRAND := SHIFT
-PRODUCT_MODEL := SHIFTphone 8
-PRODUCT_MANUFACTURER := SHIFT
-
-PRODUCT_GMS_CLIENTID_BASE := android-uniscope
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=SHIFT/otter/otter:14/SOS.6.6.20260507/20260507:user/release-keys \
