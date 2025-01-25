b73cc19db60f5fe5430a555ad6113c3f9f7dfdac
Michael Bestas <mkbestas@gmail.com>
2025-01-25 11:40:47 +0200

    otter: Update to SOS.6.2.G-RELEASE-20250624
    Change-Id: Ia27873e07f5933a00dc5719b18c20a3c298715bb

--- device.mk
+++ device.mk
@@ -132,11 +132,11 @@ PRODUCT_PACKAGES += \
 
 # e-SIM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/default-permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-permissions_com.google.android.euicc.xml \
-    $(LOCAL_PATH)/configs/privapp_allowlist_com.google.android.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp_allowlist_com.google.android.euicc.xml
+    $(LOCAL_PATH)/configs/default-permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions_com.google.android.euicc.xml \
+    $(LOCAL_PATH)/configs/privapp_allowlist_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp_allowlist_com.google.android.euicc.xml
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
 # fastbootd
 PRODUCT_PACKAGES += \
--- lineage_otter.mk
+++ lineage_otter.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := SHIFT
 PRODUCT_GMS_CLIENTID_BASE := android-uniscope
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=SHIFT/otter/otter:14/SOS.6.0.20250123/20250123:user/release-keys \
+    BuildFingerprint=SHIFT/otter/otter:14/SOS.6.2.20250624/20250624:user/release-keys \
     DeviceProduct=otter
